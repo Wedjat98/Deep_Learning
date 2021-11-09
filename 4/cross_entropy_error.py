977a5ada51ebc7f3057c47f9dbddeb6c9824eeba
狴犴HEIKAN <ame9807@gmail.com>
2021-11-09 12:13:38 +0900

    ch4

---
+++ 4/cross_entropy_error.py
@@ -0,0 +1,14 @@
+from cmath import e
+
+import numpy as np
+
+
+def cross_entropy_error(m, n):
+    delta = 1.0 * e - 7
+    return -np.sum((n * np.log(m + delta)))
+
+
+t = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+y = [0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0]
+
+print(cross_entropy_error(np.array(y), np.array(t)))
